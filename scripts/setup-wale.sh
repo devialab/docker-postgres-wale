6858830676d5346ae9d735e5ca8a624cd8e8cdf9
Alexander De Leon <me@alexdeleon.name>
2016-07-29 19:11:24 +0200

    remove envdir from archive_command

--- scripts/setup-wale.sh
+++ scripts/setup-wale.sh
@@ -2,7 +2,7 @@
 
 echo "wal_level = archive" >> /var/lib/postgresql/data/postgresql.conf
 echo "archive_mode = on" >> /var/lib/postgresql/data/postgresql.conf
-echo "archive_command = 'envdir /etc/wal-e.d/env /usr/local/bin/wal-e wal-push %p'" >> /var/lib/postgresql/data/postgresql.conf
+echo "archive_command = '/usr/local/bin/wal-e wal-push %p'" >> /var/lib/postgresql/data/postgresql.conf
 echo "archive_timeout = 60" >> /var/lib/postgresql/data/postgresql.conf
 
 su - postgres -c "crontab -l | { cat; echo \"0 9 * * * /usr/local/bin/wal-e backup-push /var/lib/postgresql/data\"; } | crontab -"
